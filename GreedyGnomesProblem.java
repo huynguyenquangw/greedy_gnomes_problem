9432bcafbe74903009130041ab0befc388e5376e
Quang Linh <quanglinh90398@gmail.com>
2022-01-06 00:38:18 +0700

    procesing input

--- GreedyGnomesProblem.java
+++ GreedyGnomesProblem.java
@@ -1,4 +1,14 @@
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.Scanner;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class GreedyGnomesProblem {
 
@@ -10,38 +20,35 @@ public class GreedyGnomesProblem {
         int rows = grid.length;
         int cols = grid[0].length;
 
+        String rock = "X";
         // return last cell
-        if (i == rows - 1 && j == cols - 1 && grid[i][j] != "X") {
+        if (i == rows - 1 && j == cols - 1 && grid[i][j] != rock) {
             // System.out.println("last cell " + grid[i][j]);
-            return parseInt(grid[i][j]);
+            return Integer.parseInt(grid[i][j]);
         }
 
-        if (i < rows - 1 && j < cols - 1 && grid[i][j] != "X") {
+        if (i < rows - 1 && j < cols - 1 && grid[i][j] != rock) {
             // System.out.println("all possible cell " + grid[i][j]);
             // System.out.println(parseInt(grid[i][j]) + dfs(i + 1, j, grid));
-            int r1 = parseInt(grid[i][j]) + dfs(i + 1, j, grid);
-            int r2 = parseInt(grid[i][j]) + dfs(i, j + 1, grid);
+            int r1 = Integer.parseInt(grid[i][j]) + dfs(i + 1, j, grid);
+            int r2 = Integer.parseInt(grid[i][j]) + dfs(i, j + 1, grid);
 
             // System.out.println(Math.max(r1, r2));
             return Math.max(r1, r2);
         }
 
-        if (i < rows - 1 && grid[i][j] != "X") {
+        if (i < rows - 1 && grid[i][j] != rock) {
 
             // System.out.println("down cell " + grid[i][j]);
-            return parseInt(grid[i][j]) + dfs(i + 1, j, grid);
+            return Integer.parseInt(grid[i][j]) + dfs(i + 1, j, grid);
         }
 
-        if (j < cols - 1 && grid[i][j] != "X") {
+        if (j < cols - 1 && grid[i][j] != rock) {
             // System.out.println("right cell" + grid[i][j]);
-            return parseInt(grid[i][j]) + dfs(i, j + 1, grid);
+            return Integer.parseInt(grid[i][j]) + dfs(i, j + 1, grid);
         }
 
         return 0;
-    }
-
-    static int parseInt(String n) {
-        return Integer.parseInt(n);
     }
 
     public static void printAllPath(String grid[][], int currRow, int currCol, String path, int golds, int steps) {
@@ -49,8 +56,10 @@ public class GreedyGnomesProblem {
         int rows = grid.length;
         int cols = grid[0].length;
 
+        String rock = "X";
+
         // base code
-        if (grid[currRow][currCol] == "X") {
+        if (grid[currRow][currCol] == rock) {
             path += "end" + grid[currRow][currCol];
             System.out.println(path + " Gold: " + golds + " Steps: " + steps);
             return;
@@ -60,7 +69,7 @@ public class GreedyGnomesProblem {
             for (int i = currCol; i <= cols - 1; i++) {
                 path += "-R:" + grid[currRow][i];
                 steps += 1;
-                golds += parseInt(grid[currRow][i]);
+                golds += Integer.parseInt(grid[currRow][i]);
             }
             System.out.println(path + " Gold: " + golds + " Steps: " + steps);
             return;
@@ -70,14 +79,14 @@ public class GreedyGnomesProblem {
             for (int i = currRow; i <= rows - 1; i++) {
                 path += "-D:" + grid[i][currCol];
                 steps += 1;
-                golds += parseInt(grid[i][currCol]);
+                golds += Integer.parseInt(grid[i][currCol]);
             }
             System.out.println(path + " Gold: " + golds + " Steps: " + steps);
             return;
         }
 
         path = path + "-" + grid[currRow][currCol];
-        golds += parseInt(grid[currRow][currCol]);
+        golds += Integer.parseInt(grid[currRow][currCol]);
         steps += 1;
 
         printAllPath(grid, currRow + 1, currCol, path + "D-", golds, steps);
@@ -93,19 +102,70 @@ public class GreedyGnomesProblem {
         }
     }
 
-    public static void inputProcessing(String[][] mat) {
+    public static String[][] inputProcessing(String[][] mat) {
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                if (mat[i][j].equals(".")) {
+                if (mat[i][j] == ".") {
                     mat[i][j] = "0";
                 }
             }
         }
-        System.out.println();
-        // return mat;
+        return mat;
+
+    };
+
+    // public static String[][] Reading2DArrayFromFile(String filename) {
+    // Scanner sc;
+    // try {
+    // sc = new Scanner(new BufferedReader(new FileReader(filename)));
+    // int rows = 25;
+    // int columns = 8;
+    // String[][] myArray = new String[rows][columns];
+    // while (sc.hasNextLine()) {
+    // for (int i = 0; i < myArray.length; i++) {
+    // String[] line = sc.nextLine().trim().split(" ");
+    // for (int j = 0; j < line.length; j++) {
+    // myArray[i][j] = line[j];
+    // }
+    // }
+    // }
+    // System.out.println(Arrays.deepToString(myArray));
+    // return myArray;
+    // } catch (FileNotFoundException e) {
+    // e.printStackTrace();
+    // }
+    // }
+
+    public static String[][] parseMap(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            String line = br.readLine();
+            String[] mapSize;
+            int row = 0;
+            if (line != null) {
+                mapSize = line.split(" ");
+                String[][] map = new String[Integer.parseInt(mapSize[0])][Integer.parseInt(mapSize[1])];
+                line = br.readLine();
+                while (line != null) {
+                    int column = 0;
+                    for (String character : line.replace(".", "0").split(" ")) {
+                        map[row][column] = character;
+                        column++;
+                    }
+                    row++;
+                    line = br.readLine();
+                }
+                // displayMap(map);
+                return map;
+            }
+        } finally {
+            br.close();
+        }
+        return null;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+
         String[][] mat1 = {
                 { ".", "X", ".", ".", "X", ".", ".", ".", ".", ".", ".", ".", ".", ".", "X",
                         ".", ".", ".", ".", ".", ".", ".", "." },
@@ -130,7 +190,7 @@ public class GreedyGnomesProblem {
                 { "1", ".", ".", "X", ".", ".", ".", ".", ".", ".", "2", ".", ".", ".", ".",
                         ".", ".", "3", ".", ".", "X", ".", "." },
                 { "2", "X", "X", "X", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".",
-                        "X", ".", ".", ".", ".", ".", "X", "." },
+                        "X", ".", ".", ".", ".", "." }
         };
 
         String mat2[][] = {
@@ -140,64 +200,30 @@ public class GreedyGnomesProblem {
                 { "X", "3", "6", "X", "0" },
                 { "1", "2", "X", "0", "1" },
         };
-        // ReadSquare square = new ReadSquare("25_8.txt");
-        // System.out.println(square.toString());
+        // displayMap(mat2);
+        System.out.println(Arrays.deepToString(mat2));
+        // printAllPath(mat2, 0, 0, "", 0, 0);
 
-        displayMap(mat2);
+        String mat3[][] = {
+                { ".", "X", ".", "13", ".", ".", "2", "." },
+                { ".", ".", "X", ".", ".", ".", ".", "." },
+                { ".", "2", ".", ".", ".", ".", "X", "X" },
+                { "X", ".", "1", ".", "X", ".", "X", "3" },
+                { "1", "2", "X", "0", "1", ".", "X", "3" },
+        };
+        // String[][] pMat3 = inputProcessing(mat3);
+        // displayMap(pMat3);
+        // printAllPath(pMat3, 0, 0, "", 0, 0);
+        try {
+            String[][] map = parseMap("25_8.txt");
+            // displayMap(map);
+            System.out.println(Arrays.deepToString(map));
 
-        displayMap(mat1);
-        inputProcessing(mat1);
-        displayMap(mat1);
+            // String[][] processMap = inputProcessing(map);
+            printAllPath(map, 0, 0, "", 0, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        printAllPath(mat1, 0, 0, "", 0, 0);
-        // System.out.println(maxPathSum(mat2));
     }
 }
-
-// class Node {
-// int[] data;
-// Node left, right;
-
-// public Node(int[] position)
-// {
-// data = position;
-// left = right = null;
-// }
-// }
-
-// class BinaryTree {
-// Node root;
-
-// int size;
-
-// BinaryTree() {root = null;}
-
-// boolean insert(int[] value) {
-// if (root == null) root = new Node(value);
-// else {
-// Node parent = null, current = root;
-// while (current != null) {
-// if (value[1] > current.data[1]){
-// parent = current;
-// current = current.left;
-// }
-// else if (value[0] > current.data[0]){
-// parent = current;
-// current = current.right;
-// }
-// else {
-// return false;
-// }
-// }
-// if (value < parent.data){
-// parent.left = new Node(value);
-// }
-// else {
-// parent.right = new Node(value);
-// }
-// }
-// size++;
-// return true;
-// }}
-
-//
--- ReadSquare.java
+++ ReadSquare.java
@@ -1,84 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+// import java.io.BufferedReader;
+// import java.io.FileReader;
 
-public class ReadSquare {
+// import java.util.Arrays;
+// import java.util.Scanner;
 
-    private int[][] matrix;
-    private int size = -1;
-    private int log10 = 0;
-    private String numberFormat;
-
-    public ReadSquare(String filename) {
-        try {
-            readFile(filename);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void readFile(String filename) throws IOException {
-        // If included in an Eclipse project.
-        InputStream stream = ClassLoader.getSystemResourceAsStream(filename);
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(stream));
-
-        // If in the same directory - Probably in your case...
-        // Just comment out the 2 lines above this and uncomment the line
-        // that follows.
-        // BufferedReader buffer = new BufferedReader(new FileReader(filename));
-
-        String line;
-        int row = 0;
-
-        while ((line = buffer.readLine()) != null) {
-            String[] vals = line.trim().split("\\s+");
-
-            // Lazy instantiation.
-            if (matrix == null) {
-                size = vals.length;
-                matrix = new int[size][size];
-                log10 = (int) Math.floor(Math.log10(size * size)) + 1;
-                numberFormat = String.format("%%%dd", log10);
-            }
-
-            for (int col = 0; col < size; col++) {
-                matrix[row][col] = Integer.parseInt(vals[col]);
-            }
-
-            row++;
-        }
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer buff = new StringBuffer();
-
-        if (matrix != null) {
-            for (int row = 0; row < size; row++) {
-                buff.append(" ");
-                for (int col = 0; col < size; col++) {
-                    buff.append(String.format(numberFormat, matrix[row][col]));
-                    if (col < size - 1) {
-                        buff.append(" | ");
-                    }
-                }
-                if (row < size - 1) {
-                    buff.append("\n");
-                    for (int col = 0; col < size; col++) {
-                        for (int i = 0; i <= log10 + 1; i++) {
-                            buff.append("-");
-                        }
-                        if (col < size - 1) {
-                            buff.append("+");
-                        }
-                    }
-                    buff.append("\n");
-                } else {
-                    buff.append("\n");
-                }
-            }
-        }
-        return buff.toString();
-    }
-}
+// public static void ReadSquare () {
+// Scanner sc = new Scanner(new BufferedReader(new FileReader("sample.txt")));
+// int rows = 4;
+// int columns = 4;
+// int [][] myArray = new int[rows][columns];while(sc.hasNextLine())
+// {
+// for (int i = 0; i < myArray.length; i++) {
+// String[] line = sc.nextLine().trim().split(" ");
+// for (int j = 0; j < line.length; j++) {
+// myArray[i][j] = Integer.parseInt(line[j]);
+// }
+// }
+// }
+// System.out.println(Arrays.deepToString(myArray));
+// }
